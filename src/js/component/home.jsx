7fc87e356704c698942a8b158ec48394f120c5dd
ehiber <ehiber5@hotmail.com>
2022-09-21 22:13:22 +0000

    todo list ciap

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,111 @@
-import React from "react";
+import React, { useState , useEffect } from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [input, setInput] = useState("")
+	const [tasks, setTasks] = useState([])
+	// [{label:"",status:"to-do"}]
+	// const [status, setStatus] = useState("to-do")
+
+	useEffect(() => {
+		const postToDo = async () =>{
+			let response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/ehiber",{
+				headers:{
+					"Content-Type":"application/json"
+				},
+				method:"POST",
+				body: JSON.stringify( []) 
+			})
+			let data = await response.json()
+			console.log(data)
+	}
+	postToDo()
+		
+	}, [])
+
+	useEffect(() => {
+		const getToDo = async () =>{
+			let response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/ehiber",{
+				headers:{
+					"Content-Type":"application/json"
+				},
+				method:"GET",
+			})
+			let data = await response.json()
+			setTasks(data)
+		
+	}
+	getToDo()
+		
+	}, [])
+
+	
+	const handleEnter = (e)=>{
+		if (e.key == "Enter"){
+			// let newTasks = [...tasks]
+			// newTasks.push(input)
+			setTasks([...tasks,{label:input,status:status}])
+			setInput("")
+		}
+	}
+
+	const handleDelete = (currentIndex)=>{
+		let newTasks = tasks.filter((task, index)=> index != currentIndex)
+		setTasks(newTasks)
+	}
+
+	const handleUpdate = (e,currentIndex)=>{
+		let newTasks = tasks.map((task, index)=> {
+			if(index == currentIndex) return {...task,status:e.target.value}
+			else return task
+		})
+		setTasks(newTasks)
+	}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			 <input 
+			 	type="text" 
+				onChange={(e)=>{setInput(e.target.value)}} 
+				value={input} 
+				onKeyDown={handleEnter}
+			/>
+
+			<select onChange={(e)=>{setStatus(e.target.value)}} name="taskStatus" id="taskStatus">
+				<option value="to do">To Do</option>
+				<option value="progress">In Progress</option>
+				<option value="done">Done</option>
+			</select>
+
+
+			 <ListGroup>
+				{
+					tasks.map((task,index)=>{
+						return (
+							<ListGroup.Item className="d-flex tasks" key={index} >
+								<p>
+									{task.label}
+								</p>
+								{/* <select onChange={(e) => handleUpdate(e,index)} name="taskStatus" id="taskStatus">
+									<option value="to do">To Do</option>
+									<option value="progress">In Progress</option>
+									<option value="done">Done</option>
+								</select> */}
+
+								<button className="button" onClick={(e) => handleDelete(index)}>
+									<span><i>X</i></span>
+								</button>
+							</ListGroup.Item>
+					)})
+				}
+			</ListGroup>
 		</div>
 	);
 };
 
 export default Home;
+
--- webpack.config.js
+++ webpack.config.js
@@ -6,68 +6,74 @@ const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 
 const port = 3000;
 let publicUrl = `ws://localhost:${port}/ws`;
-if(process.env.GITPOD_WORKSPACE_URL){
-  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
-  publicUrl = `wss://${port}-${host}/ws`;
+if (process.env.GITPOD_WORKSPACE_URL) {
+    const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
+    publicUrl = `wss://${port}-${host}/ws`;
 }
 console.log("publicUrl", publicUrl)
 
 module.exports = {
-  entry: [
-    './src/js/index.js'
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
+    entry: [
+        './src/js/index.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(css)$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }]
+            }, //css only files
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]'
+                    }
+                }
+            }, //for images
+            {
+                test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+                use: ['file-loader']
+            } //for fonts
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    devtool: "source-map",
+    devServer: {
+        port,
+        hot: true,
+        allowedHosts: "all",
+        historyApiFallback: true,
+        static: {
+            directory: path.resolve(__dirname, "dist"),
         },
-        {
-          test: /\.(css)$/, use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }]
-        }, //css only files
-        { 
-          test: /\.(png|svg|jpg|gif)$/, use: {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' } 
-          }
-        }, //for images
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
+        client: {
+            webSocketURL: publicUrl
+        },
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        // new ESLintPlugin({
+        //   files: path.resolve(__dirname, "src"),
+        // }),
+        new HtmlWebpackPlugin({
+            favicon: '4geeks.ico',
+            template: 'template.html'
+        }),
     ]
-  },
-  resolve: {
-    extensions: ['*', '.js']
-  },
-  devtool: "source-map",
-  devServer: {
-    port,
-    hot: true,
-    allowedHosts: "all",
-    historyApiFallback: true,
-    static: {
-      directory: path.resolve(__dirname, "dist"),
-    },
-    client: {
-      webSocketURL: publicUrl
-    },
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    // new ESLintPlugin({
-    //   files: path.resolve(__dirname, "src"),
-    // }),
-    new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
-        template: 'template.html'
-    }),
-  ]
 };
